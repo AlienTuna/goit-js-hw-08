2adee30850e1bc3f4f183f1d540ab4105473a6ec
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-05-17 16:43:40 +0300

    01-test

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const parrentGalleryEl = document.querySelector('ul.gallery');
+// создание разметки галереи
+parrentGalleryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+function createGallery(items) {
+
+    return items.map(({ preview, original, description }, ind) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+

f851187092102d410734374ecb3bee7e73f077fd
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-05-18 18:11:58 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
 
+// const myFrame = document.querySelector('#vimeo-player');
+// const myPlayer = new Vimeo.Player(myFrame);
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
